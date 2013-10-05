9c8f8253eb42c880b327a97e8cc94e68d56e4706
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2013-10-05 03:42:38 +0100

    Bind controls

--- controls.py
+++ controls.py
@@ -11,9 +11,12 @@ def update():
 
         if key == keys['DOWN']:
             gamelogic.direction = (0, 1)
+
         elif key == keys['LEFT']:
                 gamelogic.direction = (-1, 0)
+
         elif key == keys['RIGHT']:
             gamelogic.direction = (1, 0)
+
         elif key == keys['UP']:
             gamelogic.direction = (0, -1)
